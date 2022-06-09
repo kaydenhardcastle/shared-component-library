f4d0e72791105265c14a22616604f4f5ac5b087c
Kayden <kayden.simulated@gmail.com>
2022-06-08 19:43:58 -0500

    add landing page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<h1>Shared Component Library</h1>
+			<p>
+				This is an app containing reusable components that are responsive and
+				customizable.
+			</p>
+			<div class='gap'></div>
+			<b>Text Component</b>
+			<div class='code'>
+				<span class='grey'>&lt;</span>
+				<span class='green'>Text </span>
+
+				<span class='light-blue'>color={`{`}</span>
+				<span class='light-blue'> {`}`} </span>
+
+				<span class='light-blue'>size={`{`}</span>
+				<span class='light-blue'> {`}`} </span>
+
+				<span class='light-blue'>text={`{`}</span>
+				<span class='light-blue'> {`}`}</span>
+				<span class='grey'> /&gt;</span>
+			</div>
+		</div>
+	);
 }
 
 export default App;
